8cf1b6f6bb986b82235ae4473604390520b0ba0f
Farhaan Bukhsh <farhaan@opencraft.com>
2023-10-28 15:45:35 +0530

    test: Test if the intent is triggered
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- apps/hralarm/widget.js
+++ apps/hralarm/widget.js
@@ -6,14 +6,17 @@
   var checkHr = function(hr){
     if (hr.bpm > settings.warning && hr.bpm <= settings.upper){
       Bangle.buzz(100, 1);
+      Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.media.action.MEDIA_PLAY_FROM_SEARCH", flags:["FLAG_ACTIVITY_NEW_TASK"], categories:["android.intent.category.DEFAULT"], extra:{"query":'track:"Sittin\' on the Dock of the Bay" artist:"Otis Redding"'}}))
     }
     if (hitLimit < getTime() && hr.bpm > settings.upper){
       hitLimit = getTime() + 10;
       Bangle.buzz(2000, 1);
+      Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.media.action.MEDIA_PLAY_FROM_SEARCH", flags:["FLAG_ACTIVITY_NEW_TASK"], categories:["android.intent.category.DEFAULT"], extra:{"query":'track:"Sittin\' on the Dock of the Bay" artist:"Otis Redding"'}}))
     }
     if (hitLimit > 0 && hr.bpm < settings.lower){
       hitLimit = 0;
       Bangle.buzz(500, 1);
+      Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.media.action.MEDIA_PLAY_FROM_SEARCH", flags:["FLAG_ACTIVITY_NEW_TASK"], categories:["android.intent.category.DEFAULT"], extra:{"query":'track:"Sittin\' on the Dock of the Bay" artist:"Otis Redding"'}}))
     }
   };
   Bangle.on("HRM", checkHr);
