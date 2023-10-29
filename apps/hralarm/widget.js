3b09f925699615b8adb921ebce31f41d0299038a
Farhaan Bukhsh <farhaan@opencraft.com>
2023-10-29 13:17:50 +0530

    fix: Fix syntax
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- apps/hralarm/widget.js
+++ apps/hralarm/widget.js
@@ -8,7 +8,6 @@
       Bangle.beep();
       // Bangle.buzz(100, 1).then(() => Bangle.beep());
       // Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.media.action.MEDIA_PLAY_FROM_SEARCH", flags:["FLAG_ACTIVITY_NEW_TASK"], categories:["android.intent.category.DEFAULT"], extra:{"query":'track:"Sittin\' on the Dock of the Bay" artist:"Otis Redding"'}}))
-
       // Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.intent.action.SENDTO", data:"smsto:1234567890", extra:{"sms_body":"Hello, this is a test SMS!"}}));
       var id = NRF.getAddress().substr().substr(12).split(":");
       var url = "http://192.168.0.102:5000/sos/" + id;
@@ -21,7 +20,7 @@
       Bangle.beep();
       // Bangle.buzz(2000, 1).then(() => Bangle.beep());
       // Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.media.action.MEDIA_PLAY_FROM_SEARCH", flags:["FLAG_ACTIVITY_NEW_TASK"], categories:["android.intent.category.DEFAULT"], extra:{"query":'track:"Sittin\' on the Dock of the Bay" artist:"Otis Redding"'}}))
-      //Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.intent.action.SENDTO", data:"smsto:1234567890", extra:{"sms_body":"Hello, this is a test SMS!"}}));
+      // Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.intent.action.SENDTO", data:"smsto:1234567890", extra:{"sms_body":"Hello, this is a test SMS!"}}));
       var id = NRF.getAddress().substr().substr(12).split(":");
       var url = "http://192.168.0.102:5000/sos/" + id;
       Bangle.http(url).then(data=>{
@@ -40,6 +39,7 @@
       Bangle.http(url).then(data=>{
         console.log("Got ",data);
       });
+    }
   };
   Bangle.on("HRM", checkHr);
   Bangle.on("BTHRM", checkHr);
