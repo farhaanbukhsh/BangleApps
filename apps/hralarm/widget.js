29a4c1cd955567cb3d3a0375fc82113de69f3270
Farhaan Bukhsh <farhaan@opencraft.com>
2023-10-29 14:23:30 +0530

    fix: add vibrations
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- apps/hralarm/widget.js
+++ apps/hralarm/widget.js
@@ -5,8 +5,8 @@
   var hitLimit = 0;
   var checkHr = function(hr){
     if (hr.bpm > settings.warning && hr.bpm <= settings.upper){
-      Bangle.beep();
-      // Bangle.buzz(100, 1).then(() => Bangle.beep());
+      // Bangle.beep();
+      Bangle.buzz(100, 1);
       // Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.media.action.MEDIA_PLAY_FROM_SEARCH", flags:["FLAG_ACTIVITY_NEW_TASK"], categories:["android.intent.category.DEFAULT"], extra:{"query":'track:"Sittin\' on the Dock of the Bay" artist:"Otis Redding"'}}))
       // Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.intent.action.SENDTO", data:"smsto:1234567890", extra:{"sms_body":"Hello, this is a test SMS!"}}));
       var id = NRF.getAddress().substr().substr(12).split(":");
@@ -17,8 +17,8 @@
     }
     if (hitLimit < getTime() && hr.bpm > settings.upper){
       hitLimit = getTime() + 10;
-      Bangle.beep();
-      // Bangle.buzz(2000, 1).then(() => Bangle.beep());
+      // Bangle.beep();
+      Bangle.buzz(2000, 1);
       // Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.media.action.MEDIA_PLAY_FROM_SEARCH", flags:["FLAG_ACTIVITY_NEW_TASK"], categories:["android.intent.category.DEFAULT"], extra:{"query":'track:"Sittin\' on the Dock of the Bay" artist:"Otis Redding"'}}))
       // Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.intent.action.SENDTO", data:"smsto:1234567890", extra:{"sms_body":"Hello, this is a test SMS!"}}));
       var id = NRF.getAddress().substr().substr(12).split(":");
@@ -29,8 +29,8 @@
     }
     if (hitLimit > 0 && hr.bpm < settings.lower){
       hitLimit = 0;
-      Bangle.beep();
-      // Bangle.buzz(500, 1).then(() => Bangle.beep());
+      // Bangle.beep();
+      Bangle.buzz(500, 1);
 
       // Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.media.action.MEDIA_PLAY_FROM_SEARCH", flags:["FLAG_ACTIVITY_NEW_TASK"], categories:["android.intent.category.DEFAULT"], extra:{"query":'track:"Sittin\' on the Dock of the Bay" artist:"Otis Redding"'}}))
       // Bluetooth.println(JSON.stringify({t:"intent", target:"activity", action:"android.intent.action.SENDTO", data:"smsto:1234567890", extra:{"sms_body":"Hello, this is a test SMS!"}}));
